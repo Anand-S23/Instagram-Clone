2b6e06e911827da5d46877238e6ba55098b518b0
Anand-S23 <42609735+Anand-S23@users.noreply.github.com>
2019-10-11 09:28:19 -0400

    query posts for following

--- home/views.py
+++ home/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from post.models import Post
 
 # Create your views here.
 @login_required
 def HomeView(request):
+    #posts = Post.objects.filter(request.user__following) to desplay the post of users in following 
+    
     return render(request=request,
                   template_name='home/home.html',)
 
